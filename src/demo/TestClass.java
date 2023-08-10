abb67a4af6476805857ae955ea59ae61488a42da
0400052 <ahmad.f.ghafoor@gmail.com>
2023-08-10 15:47:02 -0400

    code updated

--- src/demo/TestClass.java
+++ src/demo/TestClass.java
@@ -6,6 +6,24 @@ public class TestClass {
 		System.out.println("Hello Guardians");
 		
 		
+		String name = "ahmad ghafoor";
+		String allUpperCase = name.toUpperCase();
+		
+		int indexOfSpace = allUpperCase.indexOf(" ");
+		
+		String lastName = name.substring(indexOfSpace);
+		String firstName = name.substring(0, indexOfSpace);
+		
+		String fullName = firstName + lastName;
+		String nameUpperCase = fullName.toUpperCase();
+		
+		System.out.println(nameUpperCase);
+		
+		
+		
+		
+		
+		
 	}
 
 }
